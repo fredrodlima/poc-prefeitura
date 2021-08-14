3d895f48390b8033728b3f380930a27c6189d574
Frederico Rodrigues <fred@devresults.com>
2021-08-14 10:43:52 -0400

    GeographiesDb: Updating PoliceReportId field type

--- dbs/GeographiesDb/CrimeOccurrences.sql
+++ dbs/GeographiesDb/CrimeOccurrences.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[Description] NVARCHAR(100) NOT NULL,
 	[PoliceDepartmentId] INT NOT NULL,
-	[PoliceReportId] NVARCHAR(100) NOT NULL,
+	[PoliceReportId] INT NOT NULL,
 	[CrimeTypeId] INT NOT NULL,
 	[Location] geography NOT NULL
 )
