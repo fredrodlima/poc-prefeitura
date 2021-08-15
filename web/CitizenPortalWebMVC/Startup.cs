758b5fd15100fdb4cc75f70002f779066324333d
Frederico Rodrigues <fred@devresults.com>
2021-08-15 13:02:23 -0400

    CitizensPortalWebMVC: Adding initial version of CitizensPortal as a web mvc project

---
+++ web/CitizenPortalWebMVC/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CitizenPortalWebMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options => 
+            {
+                options.Authority = "https://localhost:5001";
+                options.ClientId = "oidcClient";
+                options.ClientSecret = "SuperSecretPassword";
+
+                options.ResponseType = "code";
+                options.UsePkce =  true;
+                options.ResponseMode = "query";
+
+                //options.CallbackPath = "/signin-oidc"; // default redirect URI
+
+                //options.Scope.Add("oidc"); // default scope
+                //options.Scope.Add("profile"); //default scope
+                options.Scope.Add("api1.read");
+                options.SaveTokens =  true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            // app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapControllerRoute(
+            //         name: "default",
+            //         pattern: "{controller=Home}/{action=Index}/{id?}");
+            // });
+        }
+    }
+}
