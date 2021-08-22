f507755d04807540c2412fed4101aab23b4f99b9
Frederico Rodrigues <fred@devresults.com>
2021-08-22 18:33:59 -0400

    CityMonitoringDbContext: Adding AdministrativeDivisionLevel model and updating AdministrativeDivision and includnig AdministrativeDivisionLevelId property

---
+++ web/CityMonitoringAppMvc/Entities/AdministrativeDivisionLevel.cs
@@ -0,0 +1,8 @@
+﻿namespace CityMonitoringAppMvc.Entities
+{
+    public class AdministrativeDivisionLevel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
