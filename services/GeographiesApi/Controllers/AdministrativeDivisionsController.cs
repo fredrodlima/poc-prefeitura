d5b6c8e3394f4b1d01fbf719f2eacfb109d64a4e
Frederico Rodrigues <fred@devresults.com>
2021-08-22 18:31:48 -0400

    GeographiesApi: Sorting using statements and removing unused

--- services/GeographiesApi/Controllers/AdministrativeDivisionsController.cs
+++ services/GeographiesApi/Controllers/AdministrativeDivisionsController.cs
@@ -1,11 +1,10 @@
+using GeographiesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using GeographiesApi.Models;
 
 namespace GeographiesApi.Controllers
 {
