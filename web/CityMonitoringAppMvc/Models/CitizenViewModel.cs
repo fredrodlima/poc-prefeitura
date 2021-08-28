e9c7e4d14bf70e65c8dbe1f8f1741b4793d5e79e
Frederico Rodrigues <fred@devresults.com>
2021-08-28 11:14:43 -0400

    CitizensController: Adding citizen controller and ViewModels that will be helpful on CitizenLocalities page

---
+++ web/CityMonitoringAppMvc/Models/CitizenViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CityMonitoringAppMvc.Models
+{
+    public class CitizenViewModel
+    {
+        public int Id { get; set; }
+        public int CitizenTypeId { get; set; }
+        public string Name { get; set; }
+    }
+}
