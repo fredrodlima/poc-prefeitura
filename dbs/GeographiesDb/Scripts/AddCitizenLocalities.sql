f981e24198805d3610f39011559d84b0a4d28137
Frederico Rodrigues <fred@devresults.com>
2021-08-28 11:04:59 -0400

    GeographiesApi: Updating LocationTypeId in AddCitizenLocalities script

--- dbs/GeographiesDb/Scripts/AddCitizenLocalities.sql
+++ dbs/GeographiesDb/Scripts/AddCitizenLocalities.sql
@@ -16,61 +16,61 @@ INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])    
-VALUES ('Loja Armazén Dia N-01', 3, 2, geography::STGeomFromText('POINT(-122.550 47.550)', 4326));  
+VALUES ('Loja Armazén Dia N-01', 3, 5, geography::STGeomFromText('POINT(-122.550 47.550)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])  
-VALUES ('Loja Armazén Dia N-02', 3, 2, geography::STGeomFromText('POINT(-123.550 47.550)', 4326));  
+VALUES ('Loja Armazén Dia N-02', 3, 5, geography::STGeomFromText('POINT(-123.550 47.550)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])    
-VALUES ('Depósito Armazén Dia N-01', 3, 2, geography::STGeomFromText('POINT(-122.560 47.560)', 4326));  
+VALUES ('Depósito Armazén Dia N-01', 3, 4, geography::STGeomFromText('POINT(-122.560 47.560)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])  
-VALUES ('Depósito Armazén Dia N-02', 3, 2, geography::STGeomFromText('POINT(-123.560 47.560)', 4326));  
+VALUES ('Depósito Armazén Dia N-02', 3, 4, geography::STGeomFromText('POINT(-123.560 47.560)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])  
-VALUES ('Sede I Investimentos', 4, 2, geography::STGeomFromText('POINT(-123.700 47.700)', 4326));  
+VALUES ('Sede I Investimentos', 4, 6, geography::STGeomFromText('POINT(-123.700 47.700)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])  
-VALUES ('Fazenda Campo Belo N-01', 5,3, geography::STGeomFromText('POINT(-124.500 47.500)', 4326));  
+VALUES ('Fazenda Campo Belo N-01', 5, 7, geography::STGeomFromText('POINT(-124.500 47.500)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])  
-VALUES ('Fazenda Campo Belo N-02', 5, 3, geography::STGeomFromText('POINT(-123.500 48.500)', 4326));  
+VALUES ('Fazenda Campo Belo N-02', 5, 7, geography::STGeomFromText('POINT(-123.500 48.500)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])  
-VALUES ('Fazendinha N-01', 6, 3, geography::STGeomFromText('POINT(-124.500 47.500)', 4326));  
+VALUES ('Fazendinha N-01', 6, 7, geography::STGeomFromText('POINT(-124.500 47.500)', 4326));  
 GO
 
 INSERT INTO [dbo].[CitizenLocalities] ([Name],
 	[CitizenId],
 	[LocationTypeId],
 	[Location])  
-VALUES ('Fazendinha N-02', 6, 3, geography::STGeomFromText('POINT(-123.500 48.500)', 4326));  
+VALUES ('Fazendinha N-02', 6, 7, geography::STGeomFromText('POINT(-123.500 48.500)', 4326));  
 GO
