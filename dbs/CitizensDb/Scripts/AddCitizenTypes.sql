19c0cfc862310d953a2d7d8e3bfce8478c111400
Frederico Rodrigues <fred@devresults.com>
2021-08-28 11:04:15 -0400

    CitizensDb: Fixing issue in AddCitizenTypes script

--- dbs/CitizensDb/Scripts/AddCitizenTypes.sql
+++ dbs/CitizensDb/Scripts/AddCitizenTypes.sql
@@ -1,3 +1,3 @@
-﻿INSERT INTO [CitizenTypes] ([Name]) VALUES ('Pessoa Jurídica')
+﻿INSERT INTO [CitizenTypes] ([Name]) VALUES ('Pessoa Física')
 INSERT INTO [CitizenTypes] ([Name]) VALUES ('Pessoa Jurídica')
 INSERT INTO [CitizenTypes] ([Name]) VALUES ('Pessoa Rural')
