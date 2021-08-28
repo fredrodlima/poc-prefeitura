9caa179ca9a9bfd4305346a53cc72fa23558523a
Frederico Rodrigues <fred@devresults.com>
2021-08-28 16:40:21 -0400

    Projects: Updating views with portuguese language strings Also adding annotations for displayName and dataFormat of data in Views and ProjectViewModel

--- web/ProjectsAppMvc/Models/Project.cs
+++ web/ProjectsAppMvc/Models/Project.cs
@@ -1,19 +1,40 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectsAppMvc.Models
 {
-    public class Project 
+    public class Project
     {
         public int Id {get; set;}
-
+        [Display(Name = "Nome")]
         public string Name {get; set;}
-
+        [Display(Name = "Supervisor")]
         public int SupervisorId {get; set;}
-
+        [Display(Name = "Data Início")]
+        [DataType(DataType.Date)]
         public DateTime StartDate {get; set;}
+        [Display(Name = "Data Fim")]
+        [DataType(DataType.Date)]
         public DateTime? EndDate {get; set;}
+        
+    }
 
-        [Computed]
+    public class ProjectViewModel 
+    {
+        public int Id {get; set;}
+        [Display(Name = "Nome")]
+        public string Name {get; set;}
+        [Display(Name = "Supervisor")]
+        public int SupervisorId {get; set;}
+        
+        [Display(Name = "Data Início")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate {get; set;}
+        [Display(Name = "Data Fim")]
+        [DataType(DataType.Date)]
+        public DateTime? EndDate {get; set;}
+        [Display(Name = "Progresso")]
+        [DisplayFormat(DataFormatString = "{0:p2}")]
         public double Progress {get; set;}
     }
 }
