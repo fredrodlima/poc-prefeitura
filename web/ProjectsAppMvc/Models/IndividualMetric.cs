599d5aab16e5b2a8bf1f3b1882cd5dcede81249c
Frederico Rodrigues <fred@devresults.com>
2021-08-29 13:42:57 -0400

    ProjectsApp: Adding GlobalMetric and IndividualMetric models that will be updated when consuming messages

---
+++ web/ProjectsAppMvc/Models/IndividualMetric.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectsAppMvc.Models
+{
+    public class IndividualMetric
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public int PhasesNotStarted { get; set; }
+        public int PhasesInProgress { get; set; }
+        public int PhasesCompleted { get; set; }
+        public double Progress { get; set; }
+    }
+}
